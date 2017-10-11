d3a60fcfd316ffb691ee20516bed504c8606cda6
pchorak <peter.c.horak@gmail.com>
2017-10-11 11:12:32 -0400

    Tag detection working faster

--- ar_tags/src/tag_node.py
+++ ar_tags/src/tag_node.py
@@ -3,13 +3,19 @@
 # Author: PCH
 
 import rospy
+import rospkg
+import yaml
 from geometry_msgs.msg import Point
 from duckietown_msgs.msg import Tag
+import numpy as np
+import cv2
 from hampy import detect_markers
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import threading
+import os.path
 
+from cv_bridge import CvBridge      # DEBUG
 from sensor_msgs.msg import Image   # DEBUG
 
 class TagNode(object):
@@ -31,6 +37,7 @@ class TagNode(object):
         self.camera.resolution = (self.width, self.height)
         self.stream = PiRGBArray(self.camera, size=(self.width, self.height))
 
+        self.bridge = CvBridge()                                        # DEBUG
         self.frame_id = rospy.get_namespace() + "camera_optical_frame"  # DEBUG
         self.pub_img = rospy.Publisher("~image", Image, queue_size=1)   # DEBUG
         self.pub_tags = rospy.Publisher("~tags", Tag, queue_size=1)
@@ -63,10 +70,14 @@ class TagNode(object):
 
         self.width = yaml_dict['image_width']
         self.height = yaml_dict['image_height']
-        self.D = np.asarray(yaml_dict['distortion_coefficients']['data'])
-        self.K = np.asarray(yaml_dict['camera_matrix']['data'])
-        self.R = np.asarray(yaml_dict['rectification_matrix']['data'])
-        self.P = np.asarray(yaml_dict['projection_matrix']['data'])
+        tmp = yaml_dict['distortion_coefficients']
+        self.D = np.reshape(tmp['data'], (tmp['rows'], tmp['cols']))
+        tmp = yaml_dict['camera_matrix']
+        self.K = np.reshape(tmp['data'], (tmp['rows'], tmp['cols']))
+        tmp = yaml_dict['rectification_matrix']
+        self.R = np.reshape(tmp['data'], (tmp['rows'], tmp['cols']))
+        tmp = yaml_dict['projection_matrix']
+        self.P = np.reshape(tmp['data'], (tmp['rows'], tmp['cols']))
         self.map1, self.map2 = cv2.initUndistortRectifyMap(
             self.K, self.D, self.R, self.P,
             (self.width, self.height), cv2.CV_16SC2) # size=1?
@@ -76,29 +87,30 @@ class TagNode(object):
     def capture(self):
         rospy.loginfo("[%s] Start capturing." % (self.node_name))
 
-        for frame in camera.capture_continuous(self.stream, format="bgr", use_video_port=True):
+        for frame in self.camera.capture_continuous(self.stream, format="bgr", use_video_port=True):
             stamp = rospy.Time.now()
 
-            img = cv2.remap(frame.array, self.map1, self.map2, cv2.INTER_NEAREST)
+            img = cv2.remap(frame.array, self.map1, self.map2, cv2.INTER_LINEAR)
             # img = cv2.undistort(frame.array, self.K, self.D, self.R, self.P)
 
-            image_msg = Image()                             # DEBUG
-            image_msg.data = self.bridge.cv2_to_imgmsg(img) # DEBUG
-            image_msg.header.stamp = stamp                  # DEBUG
-            image_msg.header.frame_id = self.frame_id       # DEBUG
-            self.pub_img.publish(image_msg)                 # DEBUG
-            
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            _, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+            grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            _, grey = cv2.threshold(grey, 127, 255, cv2.THRESH_BINARY)
 
-            markers = detect_markers(img)
+            markers = detect_markers(grey)
 
             tag = Tag()
             tag.header.stamp = stamp
             for m in markers:
+                m.draw_contour(img) # DEBUG
                 tag.id = m.id
                 tag.point = self.image2ground(m.center)
                 self.pub_tags.publish(tag)
+
+            image_msg= self.bridge.cv2_to_imgmsg(img)   # DEBUG
+            image_msg.encoding = "bgr8"                 # DEBUG
+            image_msg.header.stamp = stamp              # DEBUG
+            image_msg.header.frame_id = self.frame_id   # DEBUG
+            self.pub_img.publish(image_msg)             # DEBUG
 
             self.stream.truncate(0)
 
